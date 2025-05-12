e0d4630fcd7bd9f880aa54424b5f31940b4f244c
DillonBarton <dillon_barton@hotmail.com>
2025-05-12 13:52:08 +0100

    added faker dependency

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,9 +1,14 @@
 "use client"
 
+import {faker} from "@faker-js/faker";
+
 const Client = () => {
 
     return (
-        <div>
+        <div className="w-full h-full flex flex-col gap-2">
+            <div className="rounded-xl shadow-lg w-[60%] h-full mx-auto p-16 flex flex-col gap-2">
+                <p>{faker.lorem.paragraphs({min: 1, max: 5})}</p>
+            </div>
         </div>
     );
 };
--- src/components/providers/SidebarProviderWrapper.tsx
+++ src/components/providers/SidebarProviderWrapper.tsx
@@ -14,7 +14,7 @@ const SidebarProviderWrapper: FunctionComponent<SidebarProviderProps> = ({childr
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                <div className="flex flex-1 flex-col gap-4 p-4">
                     {children}
                 </div>
             </SidebarInset>
